e2bc506ff609f41e8bb169ef063073c68ad37549
unknown <BT_5E7_26@BKTECH.LOCAL>
2024-01-09 15:18:01 -0500

    can't tell if this is easy or hard yet

---
+++ scripting.js
@@ -0,0 +1,7 @@
+//The following function is a modified version of the one I made
+//for my "Hot&Cold Game" project
+function addRow(tableID){
+    let tableRef = document.getElementById(tableID);
+    let newRow = table.Ref.insertRow(-1);
+    
+}
